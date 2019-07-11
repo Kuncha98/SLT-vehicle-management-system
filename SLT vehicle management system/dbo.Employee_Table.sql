be699fea20f2cba39fef0ded9a8d1d5467f2a88e
Kunchana Gayashan Ekanayaka <36447494+Kuncha98@users.noreply.github.com>
2019-07-11 20:51:06 +0530

    Employee db

---
+++ SLT vehicle management system/dbo.Employee_Table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Employee_Table] (
+    [Employee First Name]      NVARCHAR (50)  NOT NULL,
+    [Employee Last Name]       NVARCHAR (50)  NOT NULL,
+    [ Emlpoyee Address]        NVARCHAR (MAX) NOT NULL,
+	[Employee Date of Birth]   INT NOT NULL,
+    [ Emlpoyee Gender]         NVARCHAR (50)  NOT NULL,
+    [ Emlpoyee Contact Number] INT            NOT NULL,
+    [Employee E-mail]          NVARCHAR (50)  NOT NULL,
+    [Employee Password]        NVARCHAR (50)  NOT NULL, 
+    CONSTRAINT [PK_Employee_Table] PRIMARY KEY ([Employee First Name])
+);
+
